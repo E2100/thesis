d3e731f0e5460065ec4502dd7dd1393532613da3
olav <olav@bjorkoy.com>
2011-04-14 13:38:19 +0200

    working lucene wrapper

--- code/ir.rb
+++ code/ir.rb
@@ -1,3 +1,4 @@
+require 'pp'
 require 'java'
 require 'lib/metamodel/lucene'
 
@@ -5,7 +6,7 @@ M = MetaModel
 
 l = M::Lucene::API.new
 
-l.add_document(1, 'hello1')
-l.add_document(2, 'hello2')
+l.add_document(1, 'hello hello hello1')
+l.add_document(2, 'hello hello2')
 
-
+puts l.query('hello')
--- code/lib/metamodel/lucene.rb
+++ code/lib/metamodel/lucene.rb
@@ -7,22 +7,19 @@ module MetaModel
 module Lucene
   
   import org.apache.lucene.util.Version
-
   import org.apache.lucene.store.Directory
   import org.apache.lucene.store.SimpleFSDirectory
-
   import org.apache.lucene.index.IndexReader
   import org.apache.lucene.index.IndexWriter
   import org.apache.lucene.index.IndexWriterConfig
   import org.apache.lucene.index.Term
-
   import org.apache.lucene.document.Document
   import org.apache.lucene.document.Field
-
   import org.apache.lucene.analysis.standard.StandardAnalyzer
   import org.apache.lucene.queryParser.QueryParser
   import org.apache.lucene.search.IndexSearcher
-
+  
+  # https://github.com/davidx/jruby-lucene
   class API
     
     def initialize(path='data/ir/')
@@ -30,19 +27,27 @@ module Lucene
     end
    
     def dir
-      @dir ||= SimpleFSDirectory.new(java.io.File.new(@path))
+      SimpleFSDirectory.new(java.io.File.new(@path))
     end
 
     def index_available
-      @available ||= IndexReader.index_exists(dir)
+      IndexReader.index_exists(dir)
     end
     
     def index_writer_config
-      IndexWriterConfig.new(Version::LUCENE_31, StandardAnalyzer.new(Version::LUCENE_31))
+      IndexWriterConfig.new(version, analyzer)
+    end
+    
+    def index_writer
+      IndexWriter.new(dir, index_writer_config)
     end
 
-    def index_writer
-      @writer ||= IndexWriter.new(dir, index_writer_config)
+    def version
+      Version::LUCENE_31
+    end
+
+    def analyzer
+      StandardAnalyzer.new(version)
     end
 
     def field(name, content)
@@ -58,68 +63,29 @@ module Lucene
       document = Document.new
       document.add(field('text',text))
       document.add(field('id',id))
-      index_writer.update_document(existing, document)
-      #index_writer.close
+      w = index_writer
+      w.update_document(existing, document)
+      w.close
     end
 
-  end
-
-
-  # https://github.com/davidx/jruby-lucene
-  class Lucene2
-    @index_path = nil
-    
-    def initialize(an_index_path = "data/")
-      @index_path = an_index_path
-    end
-    
-    def add_documents(id_text_pair_array) # e.g., [[1,"test1"],[2,'test2']]
-      index_available = org.apache.lucene.index.IndexReader.index_exists(@index_path)
-      index_writer = org.apache.lucene.index.IndexWriter.new(
-            @index_path,
-            org.apache.lucene.analysis.standard.StandardAnalyzer.new,
-            !index_available)
-      id_text_pair_array.each {|id_text_pair|
-        term_to_delete = org.apache.lucene.index.Term.new("id", id_text_pair[0].to_s) # if it exists
-        a_document = org.apache.lucene.document.Document.new
-        a_document.add(org.apache.lucene.document.Field.new('text', id_text_pair[1],
-                         org.apache.lucene.document.Field::Store::YES,
-                         org.apache.lucene.document.Field::Index::TOKENIZED))
-        a_document.add(org.apache.lucene.document.Field.new('id', id_text_pair[0].to_s,
-                         org.apache.lucene.document.Field::Store::YES,
-                         org.apache.lucene.document.Field::Index::TOKENIZED))
-        index_writer.updateDocument(term_to_delete, a_document) # delete any old docs with same id
-      }
-      index_writer.close
-    end
-    
-    def search(query)
-      parse_query = org.apache.lucene.queryParser.QueryParser.new(
-           'text',
-           org.apache.lucene.analysis.standard.StandardAnalyzer.new)
-      query = parse_query.parse(query)
-      engine = org.apache.lucene.search.IndexSearcher.new(@index_path)
-      hits = engine.search(query).iterator
+    def query(text)
+      parser = QueryParser.new(version, 'text', analyzer)
+      parsed = parser.parse(text)
+      engine = IndexSearcher.new(dir)
+      scores = engine.search(parsed,10).scoreDocs
       results = []
-      while (hits.hasNext && hit = hits.next)
-        id = hit.getDocument.getField("id").stringValue.to_i
-        text = hit.getDocument.getField("text").stringValue
-        results << [hit.getScore, id, text]
+      scores.each do |score|
+        results << [score.score, to_s(engine.doc(score.doc))]
       end
       engine.close
       results
     end
-    
-    def delete_documents id_array # e.g., [1,5,88]
-      index_available = org.apache.lucene.index.IndexReader.index_exists(@index_path)
-      index_writer = org.apache.lucene.index.IndexWriter.new(
-            @index_path,
-            org.apache.lucene.analysis.standard.StandardAnalyzer.new,
-            !index_available)
-      id_array.each {|id|
-        index_writer.deleteDocuments(org.apache.lucene.index.Term.new("id", id.to_s))
+
+    def to_s(doc)
+      {
+        id: doc.get_field('id').string_value.to_i,
+        text: doc.get_field('text').string_value
       }
-      index_writer.close
     end
 
   end
