54a554d337efa48c798c94980907f65245c2b326
olav <olav@bjorkoy.com>
2011-04-14 16:40:22 +0200

    log hits

--- code/lib/metamodel/log.rb
+++ code/lib/metamodel/log.rb
@@ -19,6 +19,15 @@ module Log
     puts x.to_s
     puts
   end
+
+  def hits(hs)
+    hs.each_with_index do |h,i|
+      score = h.first
+      doc   = h.last
+      title = Lucene::API.new.get(doc[:id])[:text]
+      puts "#{i}: #{doc[:id]} - #{title} (#{score})"
+    end
+  end
   
   def timestamp
     Time.now.strftime("[%T] ")
