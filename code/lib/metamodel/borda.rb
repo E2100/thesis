5266055d0956f7fc5cc3657c866947dd2d4e6f54
olav <olav@bjorkoy.com>
2011-04-18 14:39:49 +0200

    less verbose

--- code/lib/metamodel/borda.rb
+++ code/lib/metamodel/borda.rb
@@ -13,16 +13,12 @@ class Borda
     s = Hash.new { 0 }
     @recs.each do |name,rec|
       r[name] = rec.recommend(@user, n).map { |item| item.item_id }
-      puts r[name]
-      puts '-'*100
     end
     r.each do |name,recs|
       recs.each do |itemid|
         s[itemid] = s[itemid] + (c - recs.index(itemid))
       end
     end
-    puts s 
-
     s
   end
 
