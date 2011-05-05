dd6f63f5f12f07f32c812b811203ee832eaa6732
olav <olav@bjorkoy.com>
2011-05-05 11:37:30 +0200

    basic aggregate recommenders: avg, min, max, median

---
+++ code/lib/metamodel/meta_basic_recommender.rb
@@ -0,0 +1,53 @@
+module MetaModel
+class MetaBasicRecommender
+
+  def initialize(task, model)
+    @task = task
+    @model = model
+  end 
+
+  def estimate_preference(userid, itemid)
+    ps = predictions(userid, itemid)
+    return Float::NAN if ps.size == 0
+    send(@task[:meta_method], ps)
+  end
+
+  def average(ps)
+    w = 1.0 / ps.size.to_f
+    ps.values.inject(0) { |sum,n| sum + (w * n) }
+  end
+
+  def max(ps)
+    ps.values.sort.last
+  end
+
+  def min(ps)
+    ps.values.sort.first
+  end
+
+  def median(ps)
+    v = ps.values.sort
+    s = v.size 
+    m = (s/2).to_i
+    if s.odd?
+      v[m]
+    else
+      (v[m-1] + v[m]).to_f / 2.0
+    end
+  end
+
+  def recommenders
+    @task[:recommenders]
+  end
+
+  def predictions(userid, itemid)
+    {}.tap do |preds|
+      recommenders.each do |name, recommender|
+        p = recommender.prediction(userid, itemid)
+        preds[name] = p unless p.nan? 
+      end
+    end
+  end
+
+end
+end
