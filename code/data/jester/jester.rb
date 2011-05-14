0b1faa99836004348603449c8041a44bf947f7eb
olav <olav@bjorkoy.com>
2011-05-14 17:47:22 +0200

    reorg jester dataset

--- code/data/jester/jester.rb
+++ code/data/jester/jester.rb
@@ -13,7 +13,10 @@ def to_user_hash(data)
     ratings.each do |r|
       i = i+1
       next if r == 99.0
-      users[u][i] = (r+10.0) / 20.0 
+      users[u][i] = (((r+10.0) / 20.0) * 4.0) + 1.0 # 1-5
+      #users[u][i] = (r+10.0) / 20.0 # 0-1
+      #users[u][i] = r # -10 - 10
+      #users[u][i] = r + 10.0 + 1.0 # 1-21
     end
   end
   users
@@ -25,10 +28,10 @@ def part(h,percent)
   h.each do |u,i|
     p1[u] = {}
     p2[u] = {}
-    is = h[u].to_a
+    is = h[u].to_a.shuffle
     x = (percent * is.size).to_i
-    i1 = is[0,x]
-    i2 = is[x,is.size-x]
+    i1 = is[0,x].shuffle
+    i2 = is[x,is.size-x].shuffle
     i1.each { |a| p1[u][a[0]] = a[1] }
     i2.each { |a| p2[u][a[0]] = a[1] }
   end
@@ -42,7 +45,7 @@ def to_rows(h)
       rows << [user,item,rating]
     end
   end
-  rows
+  rows.shuffle
 end
 
 def pname(p)
@@ -63,39 +66,58 @@ end
 
 
 data = File.open('jester.csv', 'rb').read
+puts "loaded"
 users = to_user_hash(data)
+puts "hashed"
 
-parts = part(users,0.5)
-parts1 = part(parts[0],0.5)
-parts2 = part(parts[1],0.5)
+p1 = part(users,0.8);        
+d1 = p1.last
+puts "p1 done"
 
-u1 = parts1[0]
-u2 = parts1[1]
-u3 = parts2[0]
-u4 = parts2[1]
+p2 = part(p1.first,0.75);    
+d2 = p2.last
+puts "p2 done"
 
-parts = [u1,u2,u3,u4]
-names = [:u1,:u2,:u3,:u4]
+p3 = part(p2.first,0.66);    
+d3 = p3.last
+puts "p3 done"
 
-parts.each_with_index do |u,i|
-  parts = part(u,0.8)
-  main  = parts[0]
-  test  = parts[1]
-  parts = part(main,0.6)
-  base  = parts[0]
-  meta  = parts[1]
-  
-  pp test.size
-  pp base.size
-  pp meta.size
+p4 = part(p3.first,0.50);    
+d4 = p4.last
+d5 = p4.first
+puts "p4 done"
 
-  test = to_rows(test)
-  base = to_rows(base)
-  meta = to_rows(meta)
-  
-  write(test, '20/' + names[i].to_s)
-  write(base, '50/' + names[i].to_s)
-  write(meta, '30/' + names[i].to_s)
+puts d1.size
+puts d2.size
+puts d3.size
+puts d4.size
+puts d5.size
 
-end
+puts "total: #{users.size}"
+puts "parts: #{users.size / 5}"
+
+s1 = part(d1,0.8); b1 = s1.first; t1 = s1.last; puts "s1 done"
+s2 = part(d2,0.8); b2 = s2.first; t2 = s2.last; puts "s2 done"
+s3 = part(d3,0.8); b3 = s3.first; t3 = s3.last; puts "s3 done"
+s4 = part(d4,0.8); b4 = s4.first; t4 = s4.last; puts "s4 done"
+s5 = part(d5,0.8); b5 = s5.first; t5 = s5.last; puts "s5 done"
+
+puts "writing bases"
+write(to_rows(b1), 'splits/base/1'); puts " 1"
+write(to_rows(b2), 'splits/base/2'); puts " 2"
+write(to_rows(b3), 'splits/base/3'); puts " 3"
+write(to_rows(b4), 'splits/base/4'); puts " 4"
+write(to_rows(b5), 'splits/base/5'); puts " 5"
+
+puts "writings tests"
+write(to_rows(t1), 'splits/test/1'); puts " 1"
+write(to_rows(t2), 'splits/test/2'); puts " 2"
+write(to_rows(t3), 'splits/test/3'); puts " 3"
+write(to_rows(t4), 'splits/test/4'); puts " 4"
+write(to_rows(t5), 'splits/test/5'); puts " 5"
+
+
+
+
+
 
